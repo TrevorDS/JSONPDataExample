8433ea05a5c0a0595f9d66df97637d4a6bf17b01
Trevor <59886578+TrevorDS@users.noreply.github.com>
2020-04-16 16:24:06 -0500

    Update data-example001.js

--- data/data-example001.js
+++ data/data-example001.js
@@ -14,6 +14,21 @@ showEvents({
             "location": "Athens, Greece",
             "date": "April 26",
             "map": "https://media.shermanstravel.com/Advice/1920x1080_parthenonathens_photocreditkelsychauvin.jpg"
+        },
+        {
+            "location": "Riyadh, Saudi Arabia",
+            "date": "April 29",
+            "map": "https://www.arabnews.com/sites/default/files/styles/n_670_395/public/2020/02/05/1957556-930495522.jpg?itok=LpMevhgt"
+        },
+        {
+            "location": "Cancún, Mexico",
+            "date": "May 5",
+            "map": "https://www.tripsavvy.com/thmb/8aPXMVHcNXiohsL_3a4hITJ6nBY=/3896x2922/smart/filters:no_upscale()/aerial-view-of-cancun-beach-1086671738-7b2391588e594bcb8273b1dae9455271.jpg"
+        },
+        {
+            "location": "Bolivia",
+            "date": "May 15",
+            "map": "https://www.washingtonpost.com/resizer/GcfLURIn3HzW6vMqi2SpCUas6y4=/1484x0/arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/TD4HZ2QRXEI6VESMWNGQTO6JJA.jpg"
         }
     ]
 });
