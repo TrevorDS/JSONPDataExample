de141107ab90646be85e2a8d41372060260601df
Trevor <59886578+TrevorDS@users.noreply.github.com>
2020-04-16 10:59:24 -0500

    Add files via upload

---
+++ data-example001.js
@@ -0,0 +1,19 @@
+showEvents({
+    "events": [
+        {
+            "location": "London, England",
+            "date": "April 20",
+            "map": "https://lp-cms-production.imgix.net/2019-06/55425108.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4"
+        },
+        {
+            "location": "Cairo, Egypt",
+            "date": "April 23",
+            "map": "https://www.roughguides.com/wp-content/uploads/2012/11/pyramids-cairo-egypt-shutterstock_625848488.jpg"
+        },
+        {
+            "location": "Athens, Greece",
+            "date": "April 26",
+            "map": "https://media.shermanstravel.com/Advice/1920x1080_parthenonathens_photocreditkelsychauvin.jpg"
+        }
+    ]
+});
