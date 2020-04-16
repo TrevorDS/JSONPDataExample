85bdecf5fa396dd6e5f1f15148fd725994b2ec48
Trevor <59886578+TrevorDS@users.noreply.github.com>
2020-04-16 17:45:25 -0500

    Update data-example001.js

--- data/data-example001.js
+++ data/data-example001.js
@@ -5,42 +5,48 @@ showEvents({
             "desc" : "Welcome to beautiful London, England! You can visit the giant clocktower, Big Ben, that is right beside Westminster Palace. Or, maybe, you can visit the London Eye!",
             "location" : "London, England",
             "date" : "April 20",
-            "map" : "https://lp-cms-production.imgix.net/2019-06/55425108.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4"
+            "map" : "https://lp-cms-production.imgix.net/2019-06/55425108.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4",
+            "url" : "https://trevords.github.io/JSONPDataExample/data/grabberfiles/london-grabber.js"
         },
         {
             "name" : "Trip 2",
             "desc" : "Welcome to mysterious Cairo, Eygypt! Land of the gaint pyramids, and sand. So much sand.",
             "location" : "Cairo, Egypt",
             "date" : "April 23",
-            "map" : "https://www.roughguides.com/wp-content/uploads/2012/11/pyramids-cairo-egypt-shutterstock_625848488.jpg"
+            "map" : "https://www.roughguides.com/wp-content/uploads/2012/11/pyramids-cairo-egypt-shutterstock_625848488.jpg",
+            "url" : "https://trevords.github.io/JSONPDataExample/data/grabberfiles/london-grabber.js"
         },
         {
             "name" : "Trip 3",
             "desc" : 'Welcome to the majestic Athens, Greece! Where art and beauty go hand-in-hand. "Records show the city’s origins go back to around 3,400 years ago, also making it one of the oldest cities in the world." (According to theculturetrip.com)',
             "location" : "Athens, Greece",
             "date" : "April 26",
-            "map" : "https://media.shermanstravel.com/Advice/1920x1080_parthenonathens_photocreditkelsychauvin.jpg"
+            "map" : "https://media.shermanstravel.com/Advice/1920x1080_parthenonathens_photocreditkelsychauvin.jpg",
+            "url" : "https://trevords.github.io/JSONPDataExample/data/grabberfiles/london-grabber.js"
         },
         {
             "name" : "Trip 4",
             "desc" : "Welcome to the inquisitive Riyadh, Saudi Arabia! Home of the remarkable Center Point building that has amazing architecture.",
             "location" : "Riyadh, Saudi Arabia",
             "date" : "April 29",
-            "map" : "https://www.arabnews.com/sites/default/files/styles/n_670_395/public/2020/02/05/1957556-930495522.jpg?itok=LpMevhgt"
+            "map" : "https://www.arabnews.com/sites/default/files/styles/n_670_395/public/2020/02/05/1957556-930495522.jpg?itok=LpMevhgt",
+            "url" : "https://trevords.github.io/JSONPDataExample/data/grabberfiles/london-grabber.js"
         },
         {
             "name" : "Trip 5",
             "desc" : "Welcome to the amazing Cancún, Mexico! Visit these beautiful, tropical beaches of Mexico!",
             "location" : "Cancún, Mexico",
             "date" : "May 5",
-            "map" : "https://www.tripsavvy.com/thmb/8aPXMVHcNXiohsL_3a4hITJ6nBY=/3896x2922/smart/filters:no_upscale()/aerial-view-of-cancun-beach-1086671738-7b2391588e594bcb8273b1dae9455271.jpg"
+            "map" : "https://www.tripsavvy.com/thmb/8aPXMVHcNXiohsL_3a4hITJ6nBY=/3896x2922/smart/filters:no_upscale()/aerial-view-of-cancun-beach-1086671738-7b2391588e594bcb8273b1dae9455271.jpg",
+            "url" : "https://trevords.github.io/JSONPDataExample/data/grabberfiles/london-grabber.js"
         },
         {
             "name" : "Trip 6",
             "desc" : 'Welcome to "La Paz, Bolivia"! Visit this beautiful countryside with scenic mountain!',
             "location" : "Nuestra Señora de La Paz, Bolivia",
             "date" : "May 15",
-            "map" : "https://i.pinimg.com/originals/40/66/5c/40665cc27a63aa7f81a6084d28b13c89.jpg"
+            "map" : "https://i.pinimg.com/originals/40/66/5c/40665cc27a63aa7f81a6084d28b13c89.jpg",
+            "url" : "https://trevords.github.io/JSONPDataExample/data/grabberfiles/london-grabber.js"
         }
     ]
 });
