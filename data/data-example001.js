8b897bdb4c9d68563778f597d1c94c5b4ce6555c
Trevor <59886578+TrevorDS@users.noreply.github.com>
2020-04-16 18:01:31 -0500

    Update data-example001.js

--- data/data-example001.js
+++ data/data-example001.js
@@ -6,7 +6,7 @@ showEvents({
             "location" : "London, England",
             "date" : "April 20",
             "map" : "https://lp-cms-production.imgix.net/2019-06/55425108.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4",
-            "url" : "https://trevords.github.io/JSONPDataExample/data/grabberfiles/london-grabber.js"
+            "url" : "https://trevords.github.io/JSONPDataExample/data/grabberfiles/london-grabber.json"
         },
         {
             "name" : "Trip 2",
@@ -14,7 +14,7 @@ showEvents({
             "location" : "Cairo, Egypt",
             "date" : "April 23",
             "map" : "https://www.roughguides.com/wp-content/uploads/2012/11/pyramids-cairo-egypt-shutterstock_625848488.jpg",
-            "url" : "https://trevords.github.io/JSONPDataExample/data/grabberfiles/london-grabber.js"
+            "url" : "https://trevords.github.io/JSONPDataExample/data/grabberfiles/london-grabber.json"
         },
         {
             "name" : "Trip 3",
@@ -22,7 +22,7 @@ showEvents({
             "location" : "Athens, Greece",
             "date" : "April 26",
             "map" : "https://media.shermanstravel.com/Advice/1920x1080_parthenonathens_photocreditkelsychauvin.jpg",
-            "url" : "https://trevords.github.io/JSONPDataExample/data/grabberfiles/london-grabber.js"
+            "url" : "https://trevords.github.io/JSONPDataExample/data/grabberfiles/london-grabber.json"
         },
         {
             "name" : "Trip 4",
@@ -30,7 +30,7 @@ showEvents({
             "location" : "Riyadh, Saudi Arabia",
             "date" : "April 29",
             "map" : "https://www.arabnews.com/sites/default/files/styles/n_670_395/public/2020/02/05/1957556-930495522.jpg?itok=LpMevhgt",
-            "url" : "https://trevords.github.io/JSONPDataExample/data/grabberfiles/london-grabber.js"
+            "url" : "https://trevords.github.io/JSONPDataExample/data/grabberfiles/london-grabber.json"
         },
         {
             "name" : "Trip 5",
@@ -38,7 +38,7 @@ showEvents({
             "location" : "Cancún, Mexico",
             "date" : "May 5",
             "map" : "https://www.tripsavvy.com/thmb/8aPXMVHcNXiohsL_3a4hITJ6nBY=/3896x2922/smart/filters:no_upscale()/aerial-view-of-cancun-beach-1086671738-7b2391588e594bcb8273b1dae9455271.jpg",
-            "url" : "https://trevords.github.io/JSONPDataExample/data/grabberfiles/london-grabber.js"
+            "url" : "https://trevords.github.io/JSONPDataExample/data/grabberfiles/london-grabber.json"
         },
         {
             "name" : "Trip 6",
@@ -46,7 +46,7 @@ showEvents({
             "location" : "Nuestra Señora de La Paz, Bolivia",
             "date" : "May 15",
             "map" : "https://i.pinimg.com/originals/40/66/5c/40665cc27a63aa7f81a6084d28b13c89.jpg",
-            "url" : "https://trevords.github.io/JSONPDataExample/data/grabberfiles/london-grabber.js"
+            "url" : "https://trevords.github.io/JSONPDataExample/data/grabberfiles/london-grabber.json"
         }
     ]
 });
